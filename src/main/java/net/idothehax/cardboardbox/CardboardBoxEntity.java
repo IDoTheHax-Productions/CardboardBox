a7f7a4aaf5179a8d5aeeb33765820d1975c7157d
IDoTheHax <hax@idothehax.com>
2025-04-29 08:47:17 +0200

    renamed paper fibers to cardboard and modified recipe

--- src/main/java/net/idothehax/cardboardbox/CardboardBoxEntity.java
+++ src/main/java/net/idothehax/cardboardbox/CardboardBoxEntity.java
@@ -1,27 +1,44 @@
 package net.idothehax.cardboardbox;
 
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MovementType;
+import net.minecraft.entity.*;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
+import net.minecraft.util.Arm;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-public class CardboardBoxEntity extends Entity {
-    public CardboardBoxEntity(EntityType<? extends Entity> type, World world) {
+import java.util.Collections;
+
+public class CardboardBoxEntity extends LivingEntity implements JumpingMount {
+    private float jumpStrength;
+    private boolean jumping;
+    protected boolean inAir;
+
+    public CardboardBoxEntity(EntityType<? extends LivingEntity> type, World world) {
         super(type, world);
         this.setNoGravity(false);
+        this.setHealth(this.getMaxHealth());
+    }
+
+    public static DefaultAttributeContainer.Builder createAttributes() {
+        return LivingEntity.createLivingAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2)
+                .add(EntityAttributes.GENERIC_JUMP_STRENGTH, 0.5);
     }
 
     @Override
     protected void initDataTracker(DataTracker.Builder builder) {
-
+        super.initDataTracker(builder);
     }
 
     @Override
@@ -41,7 +58,7 @@ public class CardboardBoxEntity extends Entity {
             if (forward != 0 || strafe != 0) {
                 Vec3d movement = new Vec3d(strafe, 0, forward)
                         .rotateY((float) Math.toRadians(-yaw))
-                        .multiply(0.2); // Movement speed
+                        .multiply(0.4); // Movement speed
                 this.setVelocity(movement.x, this.getVelocity().y, movement.z);
             }
 
@@ -64,11 +81,104 @@ public class CardboardBoxEntity extends Entity {
                 }
             }
 
-            // Apply friction
-            this.setVelocity(this.getVelocity().multiply(0.9));
+            // Apply friction to horizontal movement
+            this.setVelocity(this.getVelocity().multiply(0.9, 1.0, 0.9));
         }
     }
 
+    @Override
+    protected void tickControlled(PlayerEntity controllingPlayer, Vec3d movementInput) {
+        super.tickControlled(controllingPlayer, movementInput);
+        System.out.println("tickControlled called with jumpStrength: " + this.jumpStrength);
+        Vec2f vec2f = this.getControlledRotation(controllingPlayer);
+        this.setRotation(vec2f.y, vec2f.x);
+        this.prevYaw = this.bodyYaw = this.headYaw = this.getYaw();
+        if (this.isLogicalSideForUpdatingMovement()) {
+
+
+            if (this.isOnGround()) {
+                this.setInAir(false);
+                if (this.jumpStrength > 0.0F && !this.isInAir()) {
+                    this.jump(this.jumpStrength, movementInput);
+                }
+
+                this.jumpStrength = 0.0F;
+            }
+        }
+    }
+
+    protected Vec2f getControlledRotation(LivingEntity controllingPassenger) {
+        return new Vec2f(controllingPassenger.getPitch() * 0.5F, controllingPassenger.getYaw());
+    }
+
+    public boolean isInAir() {
+        return this.inAir;
+    }
+
+
+    public void setInAir(boolean inAir) {
+        this.inAir = inAir;
+    }
+
+    protected void jump(float strength, Vec3d movementInput) {
+        double jumpVelocity = this.getJumpVelocity(strength);
+        Vec3d velocity = this.getVelocity();
+        this.setVelocity(velocity.x, jumpVelocity, velocity.z);
+        this.velocityDirty = true;
+
+        // Add forward momentum if moving forward
+        if (movementInput.z > 0.0) {
+            float yawRad = (float) Math.toRadians(this.getYaw());
+            float f = (float) Math.sin(yawRad);
+            float g = (float) Math.cos(yawRad);
+            this.setVelocity(this.getVelocity().add(-0.4F * f * strength, 0.0, 0.4F * g * strength));
+        }
+    }
+
+    @Override
+    public void setJumpStrength(int strength) {
+        if (strength < 0) {
+            strength = 0;
+        } else {
+            this.jumping = true; // Mark as jumping
+        }
+
+        if (strength >= 90) {
+            this.jumpStrength = 1.0F; // Maximum jump strength
+        } else {
+            this.jumpStrength = 0.4F + 0.4F * (float) strength / 90.0F; // Scale jump strength
+        }
+    }
+
+    @Override
+    public boolean canJump() {
+        return true; // Allow jumping at all times (no saddle requirement like horses)
+    }
+
+    @Override
+    public void startJumping(int height) {
+        this.jumping = true; // Mark as jumping
+    }
+
+    @Override
+    public void stopJumping() {
+        this.jumping = false; // Reset jumping state
+    }
+
+    @Override
+    public Arm getMainArm() {
+        return null;
+    }
+
+    @Override
+    public boolean handleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource) {
+        return false;
+    }
+
+    @Override
+    public Box getBoundingBox(EntityPose pose) {
+        return new Box(-0.5, 0.0, -0.5, 0.5, 1.0, 0.5); // Adjust to match model size
+    }
 
     @Override
     public boolean collidesWith(Entity other) {
@@ -103,17 +213,32 @@ public class CardboardBoxEntity extends Entity {
     }
 
     @Override
-    protected void readCustomDataFromNbt(NbtCompound nbt) {
-
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
     }
 
     @Override
-    protected void writeCustomDataToNbt(NbtCompound nbt) {
-
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
     }
 
     @Override
     public NbtCompound writeNbt(NbtCompound nbt) {
         return super.writeNbt(nbt);
+    }
+
+    // Required by LivingEntity
+    @Override
+    public Iterable<ItemStack> getArmorItems() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public ItemStack getEquippedStack(EquipmentSlot slot) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public void equipStack(EquipmentSlot slot, ItemStack stack) {
     }
 }
--- src/main/java/net/idothehax/cardboardbox/Cardboardbox.java
+++ src/main/java/net/idothehax/cardboardbox/Cardboardbox.java
@@ -1,11 +1,14 @@
 package net.idothehax.cardboardbox;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -19,7 +22,7 @@ public class Cardboardbox implements ModInitializer {
 
     // Items
     public static final Item CARDBOARD_BOX_ITEM = new CardboardBoxItem(new Item.Settings().maxCount(1));
-    public static final Item PAPER_FIBERS = new Item(new Item.Settings().food(new FoodComponent.Builder()
+    public static final Item CARDBOARD = new Item(new Item.Settings().food(new FoodComponent.Builder()
             .alwaysEdible()
             .nutrition(1)
             .saturationModifier(0.2F)
@@ -41,15 +44,25 @@ public class Cardboardbox implements ModInitializer {
 
         registerFuels();
         registerItems();
+
+        FabricDefaultAttributeRegistry.register(CARDBOARD_BOX_ENTITY, CardboardBoxEntity.createAttributes());
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {
+            content.add(CARDBOARD_BOX_ITEM);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+            content.add(CARDBOARD);
+        });
+
     }
 
     public static void registerFuels() {
-        FuelRegistry.INSTANCE.add(PAPER_FIBERS, 240);
+        FuelRegistry.INSTANCE.add(CARDBOARD, 240);
     }
 
     public static void registerItems() {
         Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "cardboard_box"), CARDBOARD_BOX_ITEM);
-        Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "paper_fiber"), PAPER_FIBERS);
+        Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "cardboard"), CARDBOARD);
     }
 
 }
